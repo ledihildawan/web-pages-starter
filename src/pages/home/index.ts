178065d581e2cb7683b0a5e1459437c4b887fb04
ledihildawan <lhildawan@gmail.com>
2026-06-12 19:45:09 +0700

    chore: remove dead code and unused deps

--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -1,9 +1,1 @@
 import './components/features.css';
-
-(async () => {
-  try {
-    const { i18next } = await import('@/scripts/lib/i18n/runtime');
-
-    i18next.on('languageChanged', () => {});
-  } catch {}
-})();
