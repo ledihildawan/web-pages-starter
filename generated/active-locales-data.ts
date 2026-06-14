39be12e41c751e96498317e3d3760a67eca2163a
ledihildawan <lhildawan@gmail.com>
2026-06-14 13:07:49 +0700

    chore: update active-locales-data stub for new exports

--- generated/active-locales-data.ts
+++ generated/active-locales-data.ts
@@ -1,1 +1,4 @@
 export { LOCALES, LOCALE_CODES } from '../src/scripts/lib/i18n/data';
+export { LANGUAGES as ACTIVE_LANGUAGES } from '../src/scripts/lib/i18n/languages';
+export { NUMBERING_SYSTEMS as ACTIVE_NUMBERING_SYSTEMS } from '../src/scripts/lib/i18n/numbering-systems';
+export { WRITING_SYSTEMS as ACTIVE_WRITING_SYSTEMS, WRITING_SYSTEM } from '../src/scripts/lib/i18n/writing-systems';
